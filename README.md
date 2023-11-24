bcf0e1f3180de504fe2502d302ba4b0de2ccc50a
Kyungshik <kyungshikseong@gmail.com>
2023-11-24 14:47:36 +0900

    2023/11/23

---
+++ README.md
@@ -0,0 +1,5 @@
+Assertions 테스트 
+
+isSameAs: 주소 비교. 메모리 상에서 같은 객체를 참조하는지 확인    
+isEqualTo: 값 비교. 객체가 서로 같은 값을 가지고 있는지 확인    
+isInstanceOf: 타입 비교 
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,15 +6,26 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
+    //생성자 주입 : 생성자 주입을 통해서 의존관계를 설정한다. 불편, 필수, 의존관계일 때 사용된다.
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+    //private final : 꼭 값이 있어야 한다.
 
+
+
+
+    @Autowired
+    //생성자가 1개 있다면, @Autowired를 생략해도 된다.
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
+        //private final : 값 설정 (this.memberRepository) 없으면 오류 발생
     }
 
     @Override
