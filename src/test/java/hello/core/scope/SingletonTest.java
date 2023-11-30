d44ded8dfc19ef01226b8e2675bf19dd4d95273e
Kyungshik <kyungshikseong@gmail.com>
2023-11-30 22:21:47 +0900

    2023/11/29

---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,39 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class SingletonTest {
+
+    @Test
+    void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+
+        SingletonBean singletonBean1= ac.getBean(SingletonBean.class);
+        System.out.println("SingletonTest.singletonBeanFind");
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+        System.out.println("SingletonTest.singletonBeanFind");
+        Assertions.assertThat(singletonBean1).isSameAs(singletonBean2);
+
+        ac.close();
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destory() {
+            System.out.println("SingletonBean.destory");
+        }
+    }
+
+}
